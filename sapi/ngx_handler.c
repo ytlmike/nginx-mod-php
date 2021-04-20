06208831a0f95f675012edbc005097a031eaa50c
ytlmike <ytlmike@126.com>
2021-04-20 08:27:48 +0800

    add php register server variable logic

--- sapi/ngx_handler.c
+++ sapi/ngx_handler.c
@@ -93,11 +93,151 @@ php_nginx_sapi_read_cookies(void) {
     return NULL;
 }
 
+void
+nginx_php_set_header_variable(ngx_table_elt_t elt, zval *track_vars_array){
+    char *upper, *dest;
+    char *from = "-";
+    char *to = "_";
+    u_char *name = elt.key.data;
+    size_t n = elt.key.len + 1;
+    if (n == 0) {
+        return;
+    }
+    upper = emalloc(n);
+    dest = emalloc(n+5);
+    char *upper_ptr = upper;
+    while(--n) {
+        if ((char)*name == *from) {
+            *upper_ptr = *to;
+        } else {
+            *upper_ptr = (*name >= 'a' && *name <= 'z') ? (*name & ~0x20) : *name;
+        }
+        if (*name == '\0') {
+            break;
+        }
+        name++;
+        upper_ptr++;
+    }
+    *upper_ptr = '\0';
+    sprintf(dest, "HTTP_%s", upper);
+    php_register_variable_safe(dest, (char *) elt.value.data, elt.value.len, track_vars_array);
+    efree(upper);
+}
+
+void
+nginx_php_set_header_variable_name(ngx_table_elt_t elt, const char *name, const char *dest, void *default_val, zval *track_vars_array) {
+    if (ngx_strncasecmp(elt.lowcase_key, (u_char *) name, elt.key.len) == 0) {
+        php_register_variable_safe((char *)dest, (char *) elt.value.data, elt.value.len, track_vars_array);
+    } else {
+        if (default_val != NULL) {
+            php_register_variable_safe((char *)dest, (char *)default_val, 0, track_vars_array);
+        }
+    }
+}
+
+char *
+nginx_php_get_port(struct sockaddr_in *sin) {
+    ngx_uint_t port;
+    char *port_str;
+    port_str = emalloc(sizeof("65535") - 1);
+    port = ntohs(sin->sin_port);
+    sprintf(port_str, "%lu", port);
+    return port_str;
+}
+
 static void
-php_nginx_sapi_register_variables(zval *track_vars_array)
-{
+php_nginx_sapi_register_variables(zval *track_vars_array) {
+    ngx_http_request_t *r;
+    ngx_http_core_srv_conf_t *serve_conf;
+    ngx_http_php_loc_conf_t *my_conf;
+    ngx_list_part_t *part;
+    ngx_table_elt_t *header;
+    ngx_uint_t i;
+    char *method;
+
     php_import_environment_variables(track_vars_array);
-    //TODO
+
+    r = ngx_php_request;
+    serve_conf = ngx_http_get_module_srv_conf(r, ngx_http_core_module);
+    my_conf = ngx_http_get_module_loc_conf(r, ngx_http_php_module);
+
+    switch (r->method) {
+        case NGX_HTTP_GET:
+            method = (char *) "GET";
+            break;
+        case NGX_HTTP_POST:
+            method = (char *) "POST";
+            break;
+        case NGX_HTTP_HEAD:
+            method = (char *) "HEAD";
+            break;
+        case NGX_HTTP_PUT:
+            method = (char *) "PUT";
+            break;
+        case NGX_HTTP_DELETE:
+            method = (char *) "DELETE";
+            break;
+        case NGX_HTTP_OPTIONS:
+            method = (char *) "OPTIONS";
+            break;
+        case NGX_HTTP_PATCH:
+            method = (char *) "PATCH";
+            break;
+        case NGX_HTTP_TRACE:
+            method = (char *) "TRACE";
+            break;
+        default:
+            method = (char *) "";
+    }
+
+    /*
+SERVER_SOFTWARE: nginx/1.19.0 TODO
+REQUEST_SCHEME: http TODO
+DOCUMENT_ROOT: /home/ytlmike/code/blog/public
+DOCUMENT_URI: /index.php
+SCRIPT_NAME: /index.php
+PHP_SELF: /index.php TODO
+                  */
+
+    php_register_variable("REQUEST_METHOD", method, track_vars_array);
+    if (r->args.len > 0) {
+        php_register_variable_safe("QUERY_STRING", (char *) r->args.data, r->args.len, track_vars_array);
+    } else {
+        php_register_variable_safe("QUERY_STRING", (char *) "", 0, track_vars_array);
+    }
+    php_register_variable_safe("DOCUMENT_ROOT", (char *) my_conf->filename.data, my_conf->filename.len, track_vars_array); //TODO
+    php_register_variable_safe("DOCUMENT_URI", (char *) r->uri.data, r->uri.len, track_vars_array); //TODO
+    php_register_variable_safe("SCRIPT_FILENAME", (char *) my_conf->filename.data, my_conf->filename.len, track_vars_array);
+    php_register_variable_safe("SCRIPT_NAME", (char *) r->uri.data, r->uri.len, track_vars_array); //TODO
+    php_register_variable_safe("REQUEST_URI", (char *) r->uri_start,strlen((char *) r->uri_start) - strlen((char *) r->uri_end), track_vars_array);
+    php_register_variable_safe("REMOTE_ADDR", (char *) r->connection->addr_text.data, r->connection->addr_text.len, track_vars_array);
+    php_register_variable("REMOTE_PORT", nginx_php_get_port((struct sockaddr_in *) r->connection->sockaddr), track_vars_array);
+
+    ngx_str_t addr;
+    u_char     addr_str[NGX_SOCKADDR_STRLEN];
+    addr.len = NGX_SOCKADDR_STRLEN;
+    addr.data = addr_str;
+    ngx_connection_local_sockaddr(r->connection, &addr, 0);
+    php_register_variable_safe("SERVER_ADDR", (char *)addr.data, addr.len, track_vars_array);
+    php_register_variable("SERVER_PORT", nginx_php_get_port((struct sockaddr_in *) r->connection->local_sockaddr), track_vars_array);
+    php_register_variable_safe("SERVER_NAME", (char *) serve_conf->server_name.data, serve_conf->server_name.len, track_vars_array);
+    php_register_variable_safe("SERVER_PROTOCOL", (char *) r->http_protocol.data, r->http_protocol.len, track_vars_array);
+
+    part = &r->headers_in.headers.part;
+    header = part->elts;
+    for (i = 0;; i++) {
+        if (i >= part->nelts) {
+            if (part->next == NULL) {
+                break;
+            }
+            part = part->next;
+            header = part->elts;
+            i = 0;
+        }
+        nginx_php_set_header_variable_name(header[i], "content-type", "CONTENT_TYPE", "", track_vars_array);
+        nginx_php_set_header_variable_name(header[i], "content-length", "CONTENT_LENGTH", "", track_vars_array);
+        nginx_php_set_header_variable(header[i], track_vars_array);
+    }
 }
 
 static void
